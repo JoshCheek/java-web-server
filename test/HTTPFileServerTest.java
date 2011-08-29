2169d1d34ca26f3795a744558b3d878cc231cd0c
Josh Cheek <josh.cheek@gmail.com>
2011-08-29 17:35:04 -0500

    Move HTTPFileServer into its own test suite

---
+++ test/HTTPFileServerTest.java
@@ -0,0 +1,97 @@
+import java.io.*;
+import java.net.Socket;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: joshuajcheek
+ * Date: 8/29/11
+ * Time: 5:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HTTPFileServerTest extends junit.framework.TestCase{
+    public  static final int    PORT = SocketServiceTest.PORT;
+    private static final String TEST_FILE_NAME = "testfile.html";
+    private static final String TEST_FILE_CONTENTS = "<html><body><p>Hello, World!</p></body></html>";
+    private SocketService       ss;
+
+    public void setUp() throws Exception {
+        ss = new SocketService();
+    }
+
+    public void tearDown() throws Exception {
+        ss.close();
+    }
+
+    public void testServesFile() throws Exception {
+        String response = serveTestFileAndReturnResponse();
+        assertContains(response, TEST_FILE_CONTENTS);
+    }
+
+    private String serveTestFileAndReturnResponse() throws Exception {
+        createTestFile(TEST_FILE_NAME);
+        serveTestFile();
+        String response = readFromHTTPFileServer();
+        deleteTestFile(TEST_FILE_NAME);
+        return response;
+    }
+
+    private String readFromHTTPFileServer() throws IOException {
+        Socket socket = new Socket("localhost", PORT);
+        BufferedReader br = SocketService.getBufferedReader(socket);
+        String response = readResponse(br);
+        socket.close();
+        return response;
+    }
+
+    private String readResponse(BufferedReader br) throws IOException {
+        String response = "";
+        int c=0;
+        while( (c = br.read()) != -1 )
+            response += Character.toString((char)c);
+        return response;
+    }
+
+    private void serveTestFile() throws Exception {
+        ss.serve(PORT, new HTTPFileServer(TEST_FILE_NAME));
+    }
+
+    private void assertContains(String container, String containee) {
+        assertTrue("Expected \"" + container + "\" to contain \"" + containee + "\"", container.contains(containee));
+    }
+
+    private void createTestFile(String testFileName) {
+        try {
+            File file = new File(testFileName);
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+            writer.write(TEST_FILE_CONTENTS);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void deleteTestFile(String testFileName) {
+        new File(testFileName).delete();
+    }
+
+    private class HTTPFileServer implements SocketServer {
+        private String fileName;
+
+        public HTTPFileServer(String testFileName) {
+            fileName = testFileName;
+        }
+
+        public void serve(Socket s) {
+            try {
+                FileInputStream fstream = new FileInputStream(fileName);
+                DataInputStream in = new DataInputStream(fstream);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                PrintStream writer = SocketService.getPrintStream(s);
+                int c;
+                while ((c = reader.read()) != -1)
+                    writer.write(c);
+            } catch (IOException e) {
+            }
+        }
+    }
+}
--- test/SocketServiceTest.java
+++ test/SocketServiceTest.java
@@ -9,9 +9,7 @@ import java.net.Socket;
  * To change this template use File | Settings | File Templates.
  */
 public class SocketServiceTest extends junit.framework.TestCase {
-    private static final int    PORT = 1501;
-    private static final String TEST_FILE_NAME = "testfile.html";
-    private static final String TEST_FILE_CONTENTS = "<html><body><p>Hello, World!</p></body></html>";
+    public  static final int    PORT = 1501;
     private static int          threadsActive = 0;
     private int                 connections = 0;
     private SocketServer        connectionCounter;
@@ -98,35 +96,6 @@ public class SocketServiceTest extends junit.framework.TestCase {
         assertEquals(0, threadsActive);
     }
 
-    public void testServesFile() throws Exception {
-        createTestFile(TEST_FILE_NAME);
-        ss.serve(PORT, new HTTPFileServer(TEST_FILE_NAME));
-        Socket socket = new Socket("localhost", PORT);
-        BufferedReader br = SocketService.getBufferedReader(socket);
-        String response = "";
-        int c=0;
-        while( (c = br.read()) != -1 )
-            response += Character.toString((char)c);
-        socket.close();
-        deleteTestFile(TEST_FILE_NAME);
-        assertEquals(TEST_FILE_CONTENTS, response);
-    }
-
-    private void createTestFile(String testFileName) {
-        try {
-            File file = new File(testFileName);
-            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-            writer.write(TEST_FILE_CONTENTS);
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void deleteTestFile(String testFileName) {
-        new File(testFileName).delete();
-    }
-
     private void connect(int port) {
         try {
             Socket s = new Socket("localhost", port);
@@ -174,27 +143,6 @@ public class SocketServiceTest extends junit.framework.TestCase {
             try {
                 Thread.sleep(100);
             } catch (InterruptedException e) {
-            }
-        }
-    }
-
-    private class HTTPFileServer implements SocketServer {
-        private String fileName;
-
-        public HTTPFileServer(String testFileName) {
-            fileName = testFileName;
-        }
-
-        public void serve(Socket s) {
-            try {
-                FileInputStream fstream = new FileInputStream(fileName);
-                DataInputStream in = new DataInputStream(fstream);
-                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-                PrintStream writer = SocketService.getPrintStream(s);
-                int c;
-                while( (c = reader.read()) != -1 )
-                    writer.write(c);
-            } catch (IOException e) {
             }
         }
     }
