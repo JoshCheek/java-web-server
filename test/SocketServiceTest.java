971f1823adbf97ed32817532879ebce2d960fe70
Josh Cheek <josh.cheek@gmail.com>
2011-08-29 13:29:09 -0500

    Add an echoserver

--- test/SocketServiceTest.java
+++ test/SocketServiceTest.java
@@ -12,6 +12,7 @@ public class SocketServiceTest extends junit.framework.TestCase {
     private static final int    PORT = 1501;
     private int                 connections = 0;
     private SocketServer        connectionCounter;
+    private SocketService       ss;
 
     public SocketServiceTest(String name) {
         super(name);
@@ -22,33 +23,49 @@ public class SocketServiceTest extends junit.framework.TestCase {
         };
     }
 
+    public void setUp() throws Exception {
+        ss = new SocketService();
+    }
+
+    public void tearDown() throws Exception {
+        ss.close();
+    }
+
     public void testOneConnection() throws Exception {
-        SocketService ss = new SocketService();
         ss.serve(PORT, connectionCounter);
         connect(PORT);
-        ss.close();
         assertEquals(1, connections);
     }
 
     public void testManyConnections() throws Exception {
-        SocketService ss = new SocketService();
         ss.serve(PORT, connectionCounter);
         for( int i = 0; i < 10; ++i )
             connect(PORT);
-        ss.close();
         assertEquals(10, connections);
     }
 
     public void testSendMessage() throws Exception {
-        SocketService ss = new SocketService();
         ss.serve(PORT, new HelloServer());
         Socket              s       = new Socket("localhost", PORT);
         InputStream         is      = s.getInputStream();
         InputStreamReader   isr     = new InputStreamReader(is);
         BufferedReader      br      = new BufferedReader(isr);
         String              answer  = br.readLine();
-        s.close();
         assertEquals("Hello", answer);
+    }
+
+    public void testReceiveMessage() throws Exception {
+        ss.serve(PORT, new EchoService());
+        Socket              s       = new Socket("localhost", PORT);
+        InputStream         is      = s.getInputStream();
+        InputStreamReader   isr     = new InputStreamReader(is);
+        BufferedReader      br      = new BufferedReader(isr);
+        OutputStream        os      = s.getOutputStream();
+        PrintStream         ps      = new PrintStream(os);
+        ps.println("MyMessage");
+        String              answer  = br.readLine();
+        s.close();
+        assertEquals("MyMessage", answer);
     }
 
     private void connect(int port) {
@@ -70,6 +87,21 @@ public class SocketServiceTest extends junit.framework.TestCase {
             try {
                 PrintStream ps = new PrintStream(s.getOutputStream());
                 ps.println("Hello");
+            } catch (IOException e) {
+            }
+        }
+    }
+
+    private class EchoService implements SocketServer {
+        public void serve(Socket s) {
+            try {
+                InputStream         is      = s.getInputStream();
+                InputStreamReader   isr     = new InputStreamReader(is);
+                BufferedReader      br      = new BufferedReader(isr);
+                OutputStream        os      = s.getOutputStream();
+                PrintStream         ps      = new PrintStream(os);
+                String              token   = br.readLine();
+                ps.println(token);
             } catch (IOException e) {
             }
         }
