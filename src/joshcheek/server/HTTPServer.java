feb23360789f7e52e927db4070ed78ae616be8d8
Josh Cheek <josh.cheek@gmail.com>
2011-08-30 12:02:52 -0500

    Handle get and post requests, no longer stubbed

--- src/joshcheek/server/HTTPServer.java
+++ src/joshcheek/server/HTTPServer.java
@@ -1,5 +1,6 @@
 package joshcheek.server;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.Socket;
 
@@ -11,19 +12,37 @@ import java.net.Socket;
  * To change this template use File | Settings | File Templates.
  */
 public class HTTPServer implements SocketServer {
-    public void serve(Socket s) throws IOException {
 
+    private BufferedReader reader;
+    private String requestMethod;
+    private String requestURI;
+    private String requestProtocolVersion;
+
+    public void serve(Socket socket) throws IOException {
+        setReader(socket);
+        processHeader();
+    }
+
+    private void setReader(Socket socket) throws IOException {
+        reader = SocketService.getBufferedReader(socket);
     }
 
     public String requestMethod() {
-        return "GET";
+        return requestMethod;
     }
 
     public String requestURI() {
-        return "/";
+        return requestURI;
     }
 
     public String requestProtocolVersion() {
-        return "HTTP/1.0";
+        return requestProtocolVersion;
+    }
+
+    private void processHeader() throws IOException {
+        String[] firstLine = reader.readLine().split(" ");
+        requestMethod           = firstLine[0];
+        requestURI              = firstLine[1];
+        requestProtocolVersion  = firstLine[2];
     }
 }
--- test/HTTPServerTest.java
+++ test/HTTPServerTest.java
@@ -1,6 +1,10 @@
 import joshcheek.server.HTTPServer;
 import joshcheek.server.SocketService;
 
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+
 /**
  * Created by IntelliJ IDEA.
  * User: joshuajcheek
@@ -12,6 +16,14 @@ public class HTTPServerTest extends junit.framework.TestCase {
     public  static final int    PORT = SocketServiceTest.PORT;
     private SocketService       ss;
     private HTTPServer          server;
+    private static final String GET_REQUEST="GET / HTTP/1.1\r\n";
+    private static final String POST_REQUEST =  "POST /path/script.cgi HTTP/1.0\r\n" +
+                                                "From: frog@jmarshall.com\r\n" +
+                                                "User-Agent: HTTPTool/1.0\r\n" +
+                                                "Content-Type: application/x-www-form-urlencoded\r\n" +
+                                                "Content-Length: 32\r\n" +
+                                                "\r\n" +
+                                                "home=Cosby&favorite+flavor=flies\r\n";
 
     public void setUp() throws Exception {
         setSocketService();
@@ -24,26 +36,24 @@ public class HTTPServerTest extends junit.framework.TestCase {
     }
 
     // primarily getting interface expectations from http://www.w3.org/Protocols/HTTP/HTTP2.html
+    //                                           and http://jmarshall.com/easy/http/
 
-    public void testRecognizesSimpleGetRequests() throws Exception {
-        connect("GET /");
-        assertEquals("GET", server.requestMethod());
-        assertEquals("/", server.requestURI());
-    }
-
-    public void testRecognizesComplexGetRequests() throws Exception {
-        connect("GET / HTTP/1.0");
+    public void testRecognizesGetRequests() throws Exception {
+        connect(GET_REQUEST);
         assertEquals("GET",         server.requestMethod());
         assertEquals("/",           server.requestURI());
-        assertEquals("HTTP/1.0",    server.requestProtocolVersion());
+        assertEquals("HTTP/1.1",    server.requestProtocolVersion());
     }
 
-    private void connect(String requestLine) throws Exception {
-
+    public void testRecognizesPostRequest() throws Exception {
+        connect(POST_REQUEST);
+        assertEquals("POST",                server.requestMethod());
+        assertEquals("/path/script.cgi",    server.requestURI());
+        assertEquals("HTTP/1.0",            server.requestProtocolVersion());
     }
 
     private void serveServer() throws Exception {
-        ss.serve(PORT, new HTTPServer());
+        ss.serve(PORT, server);
     }
 
     private void setServer() {
@@ -58,4 +68,16 @@ public class HTTPServerTest extends junit.framework.TestCase {
         ss.close();
     }
 
+    private void connect(String requestLine) throws Exception {
+        Socket socket = new Socket("localhost", PORT);
+        writeToServer(socket, requestLine);
+        socket.close();
+        Thread.sleep(600);
+    }
+
+    private void writeToServer(Socket socket, String toWrite) throws IOException {
+        PrintStream out = SocketService.getPrintStream(socket);
+        out.write(toWrite.getBytes());
+        out.flush();
+    }
 }
