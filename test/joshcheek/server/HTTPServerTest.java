081482f5972d0b196f12a2956f3224d75cba30b0
Josh Cheek <josh.cheek@gmail.com>
2011-09-02 08:18:42 -0500

    Move the HTTPRequestHandlerImp into the test file that uses it

--- test/joshcheek/server/HTTPServerTest.java
+++ test/joshcheek/server/HTTPServerTest.java
@@ -104,4 +104,36 @@ public class HTTPServerTest extends junit.framework.TestCase {
         out.write(toWrite.getBytes());
         out.flush();
     }
+
+    public class HTTPRequestHandlerImp implements HTTPRequestHandler {
+
+        private HTTPInteraction interaction;
+        private String content = "";
+
+        public void handle(HTTPInteraction interaction) throws IOException {
+            this.interaction = interaction;
+            setContent();
+        }
+
+        public String method() {
+            return interaction.requestMethod();
+        }
+
+        public String uri() {
+            return interaction.requestUri();
+        }
+
+        public String protocolVersion() {
+            return interaction.requestProtocolVersion();
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+
+        private void setContent() {
+            interaction.setContent(content);
+        }
+    }
+
 }
